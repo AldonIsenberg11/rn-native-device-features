a57a4d97577eb80e9e4f1c69db07859e882ad581
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-04 14:11:06 -0500

    Editing the favorite place items

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,15 +1,34 @@
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import PlaceItem from "./PlaceItem";
 
 function PlacesList({places}) {
+    if (!places || places.length === 0) {
+        return (
+            <View style={styles.fallbackContainer}>
+                <Text style={styles.fallBackText}>No places found. Maybe start adding some!</Text>
+            </View>
+        );
+    }
     return (
         <FlatList
             data={places}
             keyExtractor={(item) => item.id}
-            renderItem={() =>{}}
+            renderItem={({item}) => (
+                <PlaceItem place={item}/>
+            )}
         />
     )
 }
 
 export default PlacesList;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    fallbackContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    fallBackText: {
+        fontSize: 16,
+    },
+})
--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,7 +1,10 @@
 import { StyleSheet } from "react-native"
+import PlacesList from "../components/Places/PlacesList"
 
 function AllPlaces() {
-    
+    return (
+        <PlacesList />
+    )
 }
 
 export default AllPlaces
