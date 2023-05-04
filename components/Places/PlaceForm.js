0dff6486fd3ce9f795724d0f56e936ff30587597
AldonIsenberg11 <aldon.isenberg2@gmail.com>
2023-05-04 15:29:45 -0500

    Add Place screen + Navigation

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+function PlaceForm() {
+    return (
+        <View>
+            <Text>The Place FOrm</Text>
+        </View>
+    )
+}
+
+export default PlaceForm;
+
+const styles = StyleSheet.create({});
